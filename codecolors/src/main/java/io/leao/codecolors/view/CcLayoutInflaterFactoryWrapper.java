24a1ae23b5e3a37f548a7ecb8c840dc3bcbcef35
João Martins Costa <jpmcosta@gmail.com>
2016-01-12 03:54:56 +0000

    Takes def style attrs into account when adding callbacks

--- codecolors/src/main/java/io/leao/codecolors/view/CcLayoutInflaterFactoryWrapper.java
+++ codecolors/src/main/java/io/leao/codecolors/view/CcLayoutInflaterFactoryWrapper.java
@@ -1,12 +1,26 @@
 package io.leao.codecolors.view;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CheckedTextView;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.RadioButton;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.Set;
 
@@ -73,7 +87,7 @@ public class CcLayoutInflaterFactoryWrapper implements LayoutInflater.Factory2 {
     }
 
     protected void addCodeColorCallbacks(Context context, AttributeSet attrs, View view) {
-        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.CodeColors);
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.CodeColors, getViewDefStyleAttr(view), 0);
         try {
             final int N = ta.getIndexCount();
             for (int i = 0; i < N; i++) {
@@ -109,6 +123,44 @@ public class CcLayoutInflaterFactoryWrapper implements LayoutInflater.Factory2 {
                     codeColor.addCallback(drawable, callback);
                 }
             }
+        }
+    }
+
+    /**
+     * Returns the default style attribute depending on the view class.
+     * <p/>
+     * Order matters: a {@link CheckBox} is also {@link Button}, so we have to be careful when returning the default
+     * style attribute.
+     */
+    @SuppressLint("InlinedApi")
+    private static int getViewDefStyleAttr(View view) {
+        if (view instanceof RadioButton) {
+            return android.R.attr.radioButtonStyle;
+        } else if (view instanceof CheckBox) {
+            return android.R.attr.checkboxStyle;
+        } else if (view instanceof Button) {
+            return android.R.attr.buttonStyle;
+        } else if (view instanceof MultiAutoCompleteTextView) {
+            return android.R.attr.autoCompleteTextViewStyle;
+        } else if (view instanceof AutoCompleteTextView) {
+            return android.R.attr.autoCompleteTextViewStyle;
+        } else if (view instanceof EditText) {
+            return android.R.attr.editTextStyle;
+        } else if (view instanceof CheckedTextView) {
+            return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 ?
+                    android.R.attr.checkedTextViewStyle : 0;
+        } else if (view instanceof TextView) {
+            return android.R.attr.textViewStyle;
+        } else if (view instanceof Spinner) {
+            return android.R.attr.spinnerStyle;
+        } else if (view instanceof ImageButton) {
+            return android.R.attr.imageButtonStyle;
+        } else if (view instanceof RatingBar) {
+            return android.R.attr.ratingBarStyle;
+        } else if (view instanceof SeekBar) {
+            return android.R.attr.seekBarStyle;
+        } else {
+            return 0;
         }
     }
 
