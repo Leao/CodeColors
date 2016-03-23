27b753b8017cd89fa31d603eecba507eba08cdcc
João Martins Costa <jpmcosta@gmail.com>
2016-03-23 16:51:53 +0000

    Fixes drawable invalidation forcing state change

--- codecolors/src/main/java/io/leao/codecolors/callback/CcInvalidateDrawableCallback.java
+++ codecolors/src/main/java/io/leao/codecolors/callback/CcInvalidateDrawableCallback.java
@@ -1,6 +1,9 @@
 package io.leao.codecolors.callback;
 
 import android.graphics.drawable.Drawable;
+import android.util.StateSet;
+
+import java.util.Arrays;
 
 import io.leao.codecolors.res.CcColorStateList;
 
@@ -12,12 +15,26 @@ public class CcInvalidateDrawableCallback implements CcColorStateList.AnchorCall
 
     public static void invalidate(Drawable drawable) {
         if (drawable != null) {
-            final int[] state = drawable.getState();
             // Force a state change to update the color.
-            drawable.setState(new int[]{0});
-            drawable.setState(state);
+            drawable.setState(fakeStateChange(drawable.getState()));
             // Invalidate the drawable (invalidates the view).
             drawable.invalidateSelf();
+        }
+    }
+
+    /**
+     * Returns a new state that will seem to have changed in {@link Drawable#setState(int[])}, allowing
+     * {@link Drawable#onStateChange(int[])} to be called.
+     * <p/>
+     * However, the returned state is the same state with 0 (nothing) as the last item, or the reverse of that.
+     */
+    private static int[] fakeStateChange(int[] state) {
+        if (state.length == 0) {
+            return StateSet.NOTHING;
+        } else if (state[state.length - 1] == 0) {
+            return Arrays.copyOf(state, state.length - 1);
+        } else {
+            return Arrays.copyOf(state, state.length + 1);
         }
     }
 }
